1fa74f93585564fd6762a18b31c3a503a8f83df6
Imad Syed <creeperone23@gmail.com>
2025-10-30 01:10:51 -0400

    Created Project type and list of projects

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+import { Project } from "./types";
+
+export const projects: Project[] = [
+    {
+        name: 'WordList',
+        description: 'A cross-platform mobile app to expand your vocabulary. Created using Expo and React Native.',
+        imageLink: 'https://cdn.discordapp.com/attachments/935622895009407009/1433310485213741056/feature.png?ex=690439e9&is=6902e869&hm=fdd603a43b06b8a9caff64927e6a9c28951dd4b1756f14a84d6f1eef4c525c8f&',
+        url: 'https://github.com/imadsyed333/WordListReactNative'
+    },
+    {
+        name: 'EasyChef',
+        description: 'A full-stack, end-to-end social media app for creating and sharing your favorite recipes. Built using React, Django, and SQLite.',
+        imageLink: 'https://imadsyed333.github.io/EasyChef/static/media/logo.f21d1380d69604451c0c.png',
+        url: 'https://github.com/imadsyed333/EasyChef'
+    },
+    {
+        name: 'Samosa Stuff',
+        description: 'A full-stack, end-to-end e-commerce app for a small business selling samosas and other stuff. Developed using React, Express and Prisma Postgres.',
+        imageLink: 'https://media.licdn.com/dms/image/v2/D562DAQFwAwlhDVtwtA/profile-treasury-image-shrink_800_800/B56ZoAJFWkJQAg-/0/1760938973567?e=1762405200&v=beta&t=dFgBGHRHXnh-q266X1pRSPbGyv0W_AdW4E42hvg_AQE',
+        url: 'https://github.com/imadsyed333/backend'
+    }
+]
