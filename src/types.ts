16dfdf55e48fd7e9480a21761dca0fca3e503364
Imad Syed <creeperone23@gmail.com>
2025-10-30 21:53:42 -0400

    Created PageRoute type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
+import React from "react"
+import { JSX } from "react"
+
 export type Project = {
     name: string,
     description: string,
     imageLink: string,
     url: string,
+}
+
+export type PageRoute = {
+    name: string,
+    path: string,
+    element: () => React.JSX.Element
 }
