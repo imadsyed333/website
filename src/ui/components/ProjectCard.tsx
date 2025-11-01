756cf14db5f69839b9cebf5f5642a0abe65b9f9c
Imad Syed <creeperone23@gmail.com>
2025-11-01 00:36:55 -0400

    Integrated FadeWrapper component

--- src/ui/components/ProjectCard.tsx
+++ src/ui/components/ProjectCard.tsx
@@ -1,32 +1,44 @@
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+import { Card, CardActionArea, CardContent, CardMedia, Fade, Typography } from '@mui/material'
+import React, { use, useEffect } from 'react'
 import { Project } from '../../types'
+import { FadeWrapper } from './FadeWrapper'
 
 export const ProjectCard = ({ project }: { project: Project }) => {
     const { name, description, imageLink, url } = project
+
     return (
-        <Card sx={{
-            display: 'flex',
-            height: '40%',
-            width: '25%',
-            m: 1,
-        }} variant='outlined'>
-            <CardActionArea href={url} sx={{
+        <FadeWrapper>
+            <Card sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                height: '100%',
-                width: '100%'
-            }}>
-                <CardMedia image={imageLink} sx={{
+                height: '40%',
+                width: '25%',
+                m: 1,
+                mt: 2,
+                backgroundColor: 'rgba(255,255,255,0.8)',
+                backdropFilter: 'blur(10px)',
+            }} variant='outlined'>
+                <CardActionArea href={url} sx={{
                     display: 'flex',
+                    flexDirection: 'column',
                     height: '100%',
                     width: '100%'
-                }} />
-                <CardContent>
-                    <Typography variant='h4'>{name}</Typography>
-                    <Typography variant='body1'>{description}</Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                }}>
+                    <CardMedia image={imageLink} sx={{
+                        display: 'flex',
+                        height: '100%',
+                        width: '100%'
+                    }} />
+                    <CardContent sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '100%',
+                        width: '100%'
+                    }}>
+                        <Typography variant='h4'>{name}</Typography>
+                        <Typography variant='body1'>{description}</Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </FadeWrapper>
     )
 }
--- src/ui/pages/AboutPage.tsx
+++ src/ui/pages/AboutPage.tsx
@@ -1,71 +1,51 @@
-import { Box, Card, CardMedia, Typography } from '@mui/material'
+import { Box, Card, CardContent, CardMedia, Fade, Typography } from '@mui/material'
 import React from 'react'
+import { FadeWrapper } from '../components/FadeWrapper'
 
 export const AboutPage = () => {
     return (
         <Box sx={{
             display: 'flex',
+            flexDirection: 'column',
             height: '100%',
             width: '100%',
             justifyContent: 'center',
             alignItems: 'center'
         }}>
+            <Typography sx={{
+                color: 'white',
+                mt: 1,
+            }} variant='h1'>
+                About
+            </Typography>
             <Box sx={{
                 display: 'flex',
-                height: '98%',
+                height: '100%',
                 width: '100%',
+                justifyContent: 'center',
+                alignItems: 'start'
             }}>
-                <Card sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: '100%',
-                    width: '100%',
-                    background: 'rgba(255, 255, 255, 0.1)',
-                    backdropFilter: 'blur(10px)',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                    <CardMedia sx={{
-                        display: 'flex',
-                        height: '100%',
-                        width: '90%',
-                        borderRadius: 1,
-                        m: 1,
-                    }} image='https://media.licdn.com/dms/image/v2/D5635AQErIOclGowuqQ/profile-framedphoto-shrink_200_200/B56ZjjI8nYHMAY-/0/1756157430595?e=1762322400&v=beta&t=nBAuJE_JYEGgEd8lBrExh-h8ZXs-Xp4Cdm8nf42-ZhU'>
-
-                    </CardMedia>
+                <FadeWrapper>
                     <Card sx={{
                         display: 'flex',
                         height: '100%',
                         width: '100%',
-                        background: 'rgba(0, 0, 0, 0)',
-                        m: 1,
+                        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                        backdropFilter: 'blur(10px)',
+                        mx: 1,
                     }}>
-                        <Typography sx={{
-                            color: 'white'
-                        }} variant='body1'>
-                            <p>
-                                Hello there, my name is Imad. I am a full-stack software developer specializing in React, Node, Express, and PostgreSQL. I also have experience in big data analysis and data engineering with Python.
+                        <CardContent>
 
-                            </p>
-                            <b></b>
-                            <p>
-                                At my co-op with the City of Toronto, I infamously became a "hungry developer", quickly closing down Jira tickets as I hunted bugs and implemented production-level features across the stack.
-                            </p>
-                            <b />
-                            <p>
-                                When I'm not slaying insects, I like to read, play video games, watch anime, and play soccer. Check out "Hobbies" if you want to know more.
-                            </p>
-                        </Typography>
+                        </CardContent>
                     </Card>
-                </Card>
+                </FadeWrapper>
                 <Card sx={{
                     display: 'flex',
                     height: '100%',
                     width: '100%',
-                    background: 'rgba(255, 255, 255, 0.1)',
-                    backdropFilter: 'blur(10px)',
-                }}>How are you</Card>
+                    mx: 1,
+                }}>
+                </Card>
             </Box>
         </Box>
     )
