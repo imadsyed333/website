81ec836f7334917d09ec04e0358667a5e17cbbc7
Imad Syed <creeperone23@gmail.com>
2025-11-01 00:39:15 -0400

    Implemented highlighting of nav buttons

--- src/ui/components/NavButton.tsx
+++ src/ui/components/NavButton.tsx
@@ -1,17 +1,30 @@
 import { Card, CardActionArea, Typography } from '@mui/material'
-import React from 'react'
-import { useNavigate } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
 
 export const NavButton = ({ name, link }: { name: string, link: string }) => {
     const navigate = useNavigate()
+    const location = useLocation()
+    const [selected, setSelected] = useState(false)
+
+    useEffect(() => {
+        setSelected(location.pathname === link)
+    }, [location])
+
     return (
         <Card sx={{
             display: 'flex',
             justifyContent: 'center',
             m: 1,
             backgroundColor: 'rgba(255,255,255,0)',
+            boxShadow: 0,
+            width: '10%',
+            borderBottom: (selected ? 2 : 0),
+            borderColor: 'white',
         }}>
-            <CardActionArea onClick={() => navigate(link)} sx={{
+            <CardActionArea onClick={() => {
+                navigate(link)
+            }} sx={{
                 display: 'flex',
                 height: '100%',
                 width: '100%',
